e08d2e67b8d782d511c74dc9b13ebeef654a7cde
Ethem-deli <del22021@byui.edu>
2024-06-18 18:25:13 +0300

    prep2 finished added some additional functions also

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,33 +8,72 @@ class Program
         Console.Write("What is your grade percentage? ");
         string answer = Console.ReadLine();
         int percent = int.Parse(answer);
-
+        
+        // Strech Challenge added (+ or - sign) and i have added description to make it more fun.
         string letter = "";
+        string description = "";
+        string sign = "";
 
+        // Determine the letter grade and description
         if (percent >= 90)
         {
             letter = "A";
+            description = "Excellent";
         }
-        else if  (percent >= 80)
+        else if (percent >= 80)
         {
             letter = "B";
+            description = "Very Good";
         }
-         else if (percent >= 70)
+        else if (percent >= 70)
         {
             letter = "C";
+            description = "Good";
         }
         else if (percent >= 60)
         {
             letter = "D";
+            description = "Pass";
         }
         else
         {
             letter = "F";
+            description = "Fail";
         }
 
-        Console.WriteLine($"Your grade is: {letter}");
-        
-        if (percent >= 70)
+        // Determine the sign for the grade
+        if (letter != "A" && letter != "F")
+        {
+            int lastDigit = percent % 10;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // Special cases for specific grades
+        if (letter == "B" && sign == "+")
+        {
+            description = "Very Good";
+        }
+        else if (letter == "C" && sign == "+")
+        {
+            description = "Good";
+        }
+        else if (letter == "D" && sign == "+")
+        {
+            description = "Pass";
+        }
+
+        // Display the final grade and description
+        Console.WriteLine($"Your grade is: {letter}{sign} ({description})");
+
+        // Display the pass/fail message
+        if (percent >= 60)
         {
             Console.WriteLine("You passed!");
         }
