b5bcf35b640da11fc04fef9b14117e8c28c4b6c4
Ethem-deli <del22021@byui.edu>
2024-06-18 19:44:50 +0300

    prep4 finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,76 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        // Collect numbers from the user
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        // 1st Part Compute the sum of the numbers
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        // 2nd Part Compute the average of the numbers
+        double average = (double)sum / numbers.Count;
+
+        // 3rd Part Find the maximum number in the list
+        int max = int.MinValue;
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        // 1st Stretch Challenge : to Find the smallest positive number
+        int? smallestPositive = null;
+        foreach (int num in numbers)
+        {
+            if (num > 0 && (smallestPositive == null || num < smallestPositive))
+            {
+                smallestPositive = num;
+            }
+        }
+
+        // 2nd Stretch Challenge : Sort the list of numbers
+        numbers.Sort();
+
+        // Displaying the results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+        if (smallestPositive.HasValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There are no positive numbers in the list.");
+        }
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
